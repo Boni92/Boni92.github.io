644be00b9386c8ab2e9f8440e3aa09baa1aae812
Nicolas <nicolas.borsari@gmail.com>
2021-10-22 16:50:23 -0300

    modif1

---
+++ public/js/section3.js
@@ -0,0 +1,47 @@
+let animadoX = document.querySelectorAll(".animadoX")
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i<animadoX.length; i++) {
+        let alturaAnimadoX = animadoX[i].offsetTop;
+        if(alturaAnimadoX - 500< scrollTop) {
+            animadoX[i].style.opacity=1;
+            animadoX[i].classList.add("mostrarIzquierda");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll)
+
+//===============================================
+
+let animadoXd = document.querySelectorAll(".animadoXd")
+
+function mostrarScroll2() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i<animadoXd.length; i++) {
+        let alturaAnimadoXd = animadoXd[i].offsetTop;
+        if(alturaAnimadoXd - 500< scrollTop) {
+            animadoXd[i].style.opacity=1;
+            animadoXd[i].classList.add("mostrarDerecha");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll2)
+
+//===============================================
+
+let animado = document.querySelectorAll(".animado")
+
+function mostrarScroll3() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i<animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado - 500< scrollTop) {
+            animado[i].style.opacity=1;
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll3)
