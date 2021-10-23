10803a17f764472abc7869478a7f8570321ca5c3
Nicolas <nicolas.borsari@gmail.com>
2021-10-22 22:40:23 -0300

    modif2

--- public/js/section3.js
+++ public/js/section3.js
@@ -38,10 +38,27 @@ function mostrarScroll3() {
     let scrollTop = document.documentElement.scrollTop;
     for (var i=0; i<animado.length; i++) {
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado - 500< scrollTop) {
+        if(alturaAnimado - 50 < scrollTop) {
             animado[i].style.opacity=1;
         }
     }
 }
 
 window.addEventListener('scroll', mostrarScroll3)
+
+//===============================================
+
+let animadoXa = document.querySelectorAll(".animadoXa")
+
+function mostrarScroll4() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i=0; i<animadoXa.length; i++) {
+        let alturaAnimadoXa = animadoXa[i].offsetTop;
+        if(alturaAnimadoXa - 500< scrollTop) {
+            animadoXa[i].style.opacity=1;
+            animadoXa[i].classList.add("mostrarAbajo");
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll4)
